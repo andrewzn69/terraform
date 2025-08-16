a3510550f17850f7cd468127759569ab3b4b43d5
andrewzn <ondrejzeman23@gmail.com>
2025-08-16 02:14:35 +0200

    add outputs for oci load balancer

--- oracle-talos/modules/network/outputs.tf
+++ oracle-talos/modules/network/outputs.tf
@@ -32,3 +32,43 @@ output "subnet_cidr_block" {
   value       = oci_core_subnet.talos_subnet.cidr_block
   description = "CIDR block of the subnet"
 }
+
+output "network_load_balancer_id" {
+  value       = oci_network_load_balancer_network_load_balancer.controlplane_load_balancer.id
+  description = "OCID of the network load balancer"
+}
+
+output "network_load_balancer_ip_addresses" {
+  value       = oci_network_load_balancer_network_load_balancer.controlplane_load_balancer.ip_addresses
+  description = "IP addresses of the network load balancer"
+}
+
+output "talos_backend_set_id" {
+  value       = oci_network_load_balancer_backend_set.talos_backend_set.id
+  description = "OCID of the talos backend set"
+}
+
+output "talos_backend_set_name" {
+  value       = oci_network_load_balancer_backend_set.talos_backend_set.name
+  description = "Name of the talos backend set"
+}
+
+output "controlplane_backend_set_id" {
+  value       = oci_network_load_balancer_backend_set.controlplane_backend_set.id
+  description = "OCID of the controlplane backend set"
+}
+
+output "controlplane_backend_set_name" {
+  value       = oci_network_load_balancer_backend_set.controlplane_backend_set.name
+  description = "Name of the controlplane backend set"
+}
+
+output "talos_listener_id" {
+  value       = oci_network_load_balancer_listener.talos_listener.id
+  description = "OCID of the talos listener"
+}
+
+output "controlplane_listener_id" {
+  value       = oci_network_load_balancer_listener.controlplane_listener.id
+  description = "OCID of the controlplane listener"
+}
