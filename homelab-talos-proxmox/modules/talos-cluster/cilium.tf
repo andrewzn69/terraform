fc1f5ce97362a90e9204f141e28f3ccc83fd8465
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-02-18 17:52:59 +0100

    add kubernetes version to cilium

--- homelab-talos-proxmox/modules/talos-cluster/cilium.tf
+++ homelab-talos-proxmox/modules/talos-cluster/cilium.tf
@@ -1,9 +1,10 @@
 data "helm_template" "cilium" {
-  name       = "cilium"
-  repository = "https://helm.cilium.io/"
-  chart      = "cilium"
-  namespace  = "kube-system"
-  version    = var.cilium_version
+  name         = "cilium"
+  repository   = "https://helm.cilium.io/"
+  chart        = "cilium"
+  namespace    = "kube-system"
+  version      = var.cilium_version
+  kube_version = var.kubernetes_version
 
   values = [file("${path.module}/values/cilium.yaml")]
 }
