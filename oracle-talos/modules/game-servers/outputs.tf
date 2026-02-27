ceefb7fb916627cd3e2e7252a062208ee5c18f42
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-02-28 00:02:41 +0100

    remove game-server module and remove references

--- oracle-talos/modules/game-servers/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "terraria_backend_set_name" {
-  value       = oci_network_load_balancer_backend_set.terraria.name
-  description = "Name of the terraria backend set"
-}
-
-output "terraria_listener_name" {
-  value       = oci_network_load_balancer_listener.terraria.name
-  description = "Name of the terraria listener"
-}
