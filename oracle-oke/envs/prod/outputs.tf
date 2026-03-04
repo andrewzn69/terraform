fc32dc980684892fc801701c8a262f7db979f6e3
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-03-04 02:22:45 +0100

    expose kubeconfig as root output

--- oracle-oke/envs/prod/outputs.tf
+++ oracle-oke/envs/prod/outputs.tf
@@ -7,3 +7,9 @@ output "cluster_endpoint" {
   description = "Public endpoint of the OKE cluster"
   value       = module.oke.cluster_endpoint
 }
+
+output "kubeconfig" {
+  description = "Kubeconfig for the OKE cluster"
+  value       = module.oke.kubeconfig
+  sensitive   = true
+}
