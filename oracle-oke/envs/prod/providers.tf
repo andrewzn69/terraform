4da5732d1de95bdc6b499fdd7bb3777e7bf3e14e
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-03-04 01:38:39 +0100

    use oci cli exec block for helm and kubernetes provider auth

--- oracle-oke/envs/prod/providers.tf
+++ oracle-oke/envs/prod/providers.tf
@@ -6,12 +6,54 @@ provider "oci" {
   region       = var.region
 }
 
+locals {
+  kubeconfig   = try(yamldecode(module.oke.kubeconfig), null)
+  cluster_host = try(local.kubeconfig.clusters[0].cluster.server, "")
+  cluster_ca   = try(base64decode(local.kubeconfig.clusters[0].cluster["certificate-authority-data"]), "")
+}
+
 provider "helm" {
-  kubernetes = {
-    config_raw = module.oke.kubeconfig
+  kubernetes {
+    host                   = local.cluster_host
+    cluster_ca_certificate = local.cluster_ca
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "oci"
+      args = [
+        "ce", "cluster", "generate-token",
+        "--cluster-id", module.oke.cluster_id,
+        "--region", var.region
+      ]
+      env = {
+        OCI_CLI_USER        = var.user_ocid
+        OCI_CLI_TENANCY     = var.tenancy_ocid
+        OCI_CLI_FINGERPRINT = var.fingerprint
+        OCI_CLI_KEY_CONTENT = var.oci_private_key
+        OCI_CLI_REGION      = var.region
+      }
+    }
   }
 }
 
 provider "kubernetes" {
-  config_raw = module.oke.kubeconfig
+  host                   = local.cluster_host
+  cluster_ca_certificate = local.cluster_ca
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "oci"
+    args = [
+      "ce", "cluster", "generate-token",
+      "--cluster-id", module.oke.cluster_id,
+      "--region", var.region
+    ]
+    env = {
+      OCI_CLI_USER        = var.user_ocid
+      OCI_CLI_TENANCY     = var.tenancy_ocid
+      OCI_CLI_FINGERPRINT = var.fingerprint
+      OCI_CLI_KEY_CONTENT = var.oci_private_key
+      OCI_CLI_REGION      = var.region
+    }
+  }
 }
