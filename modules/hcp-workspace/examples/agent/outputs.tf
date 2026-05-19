5d08eb8c144dc8e3038473a14e9d4e686696de80
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-05-19 15:15:30 +0200

    add agent example to hcp-workspace module

---
+++ modules/hcp-workspace/examples/agent/outputs.tf
@@ -0,0 +1,9 @@
+output "workspace_id" {
+  description = "ID of the created workspace"
+  value       = module.workspace.workspace_id
+}
+
+output "workspace_name" {
+  description = "Name of the created workspace"
+  value       = module.workspace.workspace_name
+}
