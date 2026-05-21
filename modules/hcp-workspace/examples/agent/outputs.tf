95425ff1190624802f7435205bc7e5997f3397a1
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-05-21 12:25:02 +0200

    delete hcp-workspace module

--- modules/hcp-workspace/examples/agent/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "workspace_id" {
-  description = "ID of the created workspace"
-  value       = module.workspace.workspace_id
-}
-
-output "workspace_name" {
-  description = "Name of the created workspace"
-  value       = module.workspace.workspace_name
-}
